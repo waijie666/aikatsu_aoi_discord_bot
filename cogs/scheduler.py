e83bc44851bcf8add4dbbd0010dd2434984d687c
waijie666 <suicune5050@hotmail.com>
2019-01-12 23:21:32 +0800

    Added scheduler to send message to channels

--- cogs/scheduler.py
+++ cogs/scheduler.py
@@ -7,21 +7,45 @@ import sys, traceback
 class SchedulerCog:
     def __init__(self, bot):
         self.bot = bot
-        bot.apscheduler.add_job(self.change_client_presence, trigger="cron", minute="*/5", id="change_client_presence", replace_existing=True)
-    
+        bot.apscheduler.add_job(
+            self.change_client_presence,
+            trigger="cron",
+            minute="*/5",
+            id="change_client_presence",
+            replace_existing=True,
+        )
+
     async def change_client_presence(self):
         client_presence_list = ["Aikatsu Friends", "Aikatsu Stars", "Aikatsu"]
         client_presence_choice = random.choice(client_presence_list)
-        await self.bot.change_presence(activity=discord.Game(name=client_presence_choice))
+        await self.bot.change_presence(
+            activity=discord.Game(name=client_presence_choice)
+        )
+
+    async def message_send(self, channel, message):
+        await channel.send(message)
 
     # Hidden means it won't show up on the default help.
-    @commands.command(name="add_job", hidden=True)
+    @commands.command(name="listjob", hidden=True)
     @commands.is_owner()
-    async def add_job(self, ctx, *, cog: str):
-        """Command which Loads a Module.
-        Remember to use dot path. e.g: cogs.owner"""
-        pass
+    async def listjob(self, ctx):
+        for job in self.bot.apscheduler.get_jobs():
+            await ctx.send(
+                "name: %s, trigger: %s, next run: %s"
+                % (job.name, job.trigger, job.next_run_time)
+            )
 
+    @commands.command(name="schedule_message", hidden=True)
+    @commands.is_owner()
+    async def schedule_message(
+        self, ctx, channel: discord.TextChannel, datetime: str, message: str
+    ):
+        self.bot.apscheduler.add_job(
+            self.message_send,
+            trigger="date",
+            run_date=datetime,
+            kwargs={"channel": channel, "message": message},
+        )
 
 
 def setup(bot):
